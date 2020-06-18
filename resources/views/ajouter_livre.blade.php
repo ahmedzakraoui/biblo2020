8e1cfb9b42dc24cb9bb4b2cd36a6aec312fa0620
Brahim <you@example.com>
2020-06-18 19:00:39 +0200

    add form

--- resources/views/ajouter_livre.blade.php
+++ resources/views/ajouter_livre.blade.php
@@ -6,18 +6,11 @@
     <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
   </div>
   <div class="form-group">
-    <label for="exampleFormControlSelect2">Catégorie</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>Informatique</option>
-      <option>Mathématique</option>
-    </select>
-  </div>
-  
-  <div class="form-group">
     <label for="exampleFormControlTextarea1">Description</label>
     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
-  
+  <div class="form-group">
+  <input type="submit" class="btn btn-primary" value="Ajouter">
+  </div>
 </form>
-
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -83,7 +83,9 @@
   <div class="container">
     <!-- Example row of columns -->
     <div class="row">
+    <div class="col-md-12">
    @yield('content')
+   </div>
     </div>
 
     <hr>
